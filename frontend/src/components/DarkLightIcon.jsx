8702a0ffdf2f9b9e567a500ddf98081ba3ea38a6
CamdzicAlden <aldencamdzic3@gmail.com>
2026-01-01 13:26:49 +0100

    feat: Add dark/light mode icon

---
+++ frontend/src/components/DarkLightIcon.jsx
@@ -0,0 +1,11 @@
+import iconStyle from "../css/DarkLightIcon.module.css";
+
+function DarkLightIcon() {
+  return (
+    <div className={iconStyle.root}>
+      <img src="icons/SunEmpty.svg" alt="S" />
+    </div>
+  );
+}
+
+export default DarkLightIcon;
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import TicTacToe from "../components/TicTacToe";
 import NavigationButton from "../components/NavigationButton";
 
 import pageStyle from "../css/LandingPage.module.css";
+import DarkLightIcon from "../components/DarkLightIcon";
 
 function LandingPage() {
   return (
     <div className={pageStyle.root}>
       <TicTacToe className="topLeft" />
       <TicTacToe className="bottomRight" />
+      <DarkLightIcon />
 
       <NavigationButton content={"1 PLAYER"} className="darkMode" />
       <NavigationButton content={"2 PLAYERS"} className="darkMode" />

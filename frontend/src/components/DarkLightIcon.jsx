8079fbf14f590d3ec498a161d30300e044732333
CamdzicAlden <aldencamdzic3@gmail.com>
2026-01-02 13:37:33 +0100

    feat: Add hover effect on dark/light icons

--- frontend/src/components/DarkLightIcon.jsx
+++ frontend/src/components/DarkLightIcon.jsx
@@ -1,15 +1,32 @@
 import iconStyle from "../css/DarkLightIcon.module.css";
 import { useTheme } from "../contexts/ThemeContext.jsx";
+import { useEffect, useState } from "react";
 
 function DarkLightIcon() {
   const { theme, toggleTheme } = useTheme();
+  const [hover, setHover] = useState(false);
+  const [image, setImage] = useState(" ");
+
+  useEffect(() => {
+    setImage(
+      theme === "dark"
+        ? hover
+          ? "/icons/SunFill.svg"
+          : "/icons/SunEmpty.svg"
+        : hover
+        ? "/icons/MoonFill.svg"
+        : "/icons/MoonEmpty.svg"
+    );
+  }, [hover, theme]);
 
   return (
     <div className={iconStyle.root}>
       <img
-        src={theme === "dark" ? "/icons/SunEmpty.svg" : "/icons/MoonEmpty.svg"}
+        src={image}
         alt="S"
         onClick={toggleTheme}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       />
     </div>
   );

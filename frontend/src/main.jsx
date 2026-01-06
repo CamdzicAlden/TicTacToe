32a9a0679e9e6cc451cc6a73ae54d3a5d399bbc9
CamdzicAlden <aldencamdzic3@gmail.com>
2026-01-06 20:21:05 +0100

    fix: Fix 404 error on reload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ThemeProvider from "./contexts/ThemeContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/TicTacToe">
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );

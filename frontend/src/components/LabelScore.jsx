829633f1ba922999687848fe6698a8d7d535b0b0
CamdzicAlden <aldencamdzic3@gmail.com>
2026-02-09 09:27:12 +0100

    fix: Change starting player every time playing board resets

--- frontend/src/components/LabelScore.jsx
+++ frontend/src/components/LabelScore.jsx
@@ -4,7 +4,11 @@ import styles from "../css/LabelScore.module.css";
 function LabelScore({ label, score, textColor }) {
   return (
     <div className={styles.root}>
+<<<<<<< HEAD
       <p className={textColor ? styles[textColor] : ""}>{label}</p>
+=======
+      <p className={styles[textColor]}>{label}</p>
+>>>>>>> 27d7add (fix: Change starting player every time playing board resets)
       <p>{score}</p>
     </div>
   );
